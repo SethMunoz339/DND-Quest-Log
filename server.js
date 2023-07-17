2e06d256839f8614c02c39aa72d9b719dd7a7dfa
drteeth <drteeth339@gmail.com>
2023-07-17 11:14:19 -0400

    fixed routes

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const path = require('path');
-const {
-  getNotes,
-  saveNote,
-  deleteNote
-} = require('./public/assets/js/index'); 
-
 const app = express();
 const PORT = 3001;
-
+const fs = require('fs');
+const utils = require('util')
+const readFile = utils.promisify(fs.readFile)
+const writeFile = utils.promisify(fs.writeFile)
+const { v4: uuidv4 } = require('uuid');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+function getNotes() {
+  return readFile('db/db.json', 'utf-8').then(rawNotes => [].concat(JSON.parse(rawNotes)))
+}
+
+function saveNotes(notes) {
+  return writeFile('db/db.json', JSON.stringify(notes))
+}
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
@@ -20,7 +26,8 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', async (req, res) => {
   try {
     const notes = await getNotes();
-    res.json(notes);
+    return res.json(notes);
+  
   } catch (error) {
     res.status(500).json({ error: 'Failed to get notes' });
   }
@@ -28,9 +35,12 @@ app.get('/api/notes', async (req, res) => {
 
 app.post('/api/notes', async (req, res) => {
   try {
-    const newNote = req.body;
-    await saveNote(newNote);
-    res.status(200).send();
+    const newNote = {title: req.body.title, text: req.body.text, id:uuidv4()};
+    const oldNotes = await getNotes()
+    const noteArray = [...oldNotes, newNote]
+    console.log(noteArray)
+    await saveNotes(noteArray);
+    res.status(200).json({message: 'succesfully saved notes'})
   } catch (error) {
     res.status(500).json({ error: 'Failed to save note' });
   }
@@ -39,15 +49,16 @@ app.post('/api/notes', async (req, res) => {
 app.delete('/api/notes/:id', async (req, res) => {
   try {
     const noteId = req.params.id;
-    await deleteNote(noteId);
-    res.status(200).send();
+    const oldArray = await getNotes();
+    const newArray = oldArray.filter(note => note.id !== noteId);
+    saveNotes(newArray).then(() => res.status(200).send());
   } catch (error) {
     res.status(500).json({ error: 'Failed to delete note' });
   }
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () =>
